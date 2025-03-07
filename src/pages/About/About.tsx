50223fd917a0fe3975b2202e1fae3dd902300174
amil <huawe@HUAWEI.(none)>
2025-03-07 13:28:50 +0400

    react icon added

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -1,8 +1,23 @@
-
+import "./About.css";
+import { GiMusicSpell } from "react-icons/gi";
 function About() {
   return (
-    <div>About</div>
-  )
+    <div className="about">
+      <div className="logo">
+
+      </div>
+      <div className="info">
+        ShahMovie is a platform designed for discovering the latest and most
+        popular movies, TV series, and documentaries. Driven by our passion for
+        cinema, we aim to bring the best content to our audience. With our
+        user-friendly interface, you can easily find your favorite genres,
+        explore trending productions, and access the latest releases quickly. We
+        are constantly evolving to enhance your entertainment experience and
+        provide the best service possible. Unlock the world of cinema with
+        ShahMovie and enjoy your favorite productions!
+      </div>
+    </div>
+  );
 }
 
-export default About
+export default About;
